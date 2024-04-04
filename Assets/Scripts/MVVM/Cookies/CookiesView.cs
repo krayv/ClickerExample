ec8691766b30846f78056578b2a84fbf63c28432
krayv <30841065+krayv@users.noreply.github.com>
2024-04-04 19:46:32 +0800

    Add Click Animation

--- Assets/Scripts/MVVM/Cookies/CookiesView.cs
+++ Assets/Scripts/MVVM/Cookies/CookiesView.cs
@@ -6,6 +6,8 @@ using UniRx;
 using TMPro;
 using Zenject;
 using System.Numerics;
+using DG.Tweening;
+using System;
 
 public class CookiesView : View
 {
@@ -15,7 +17,7 @@ public class CookiesView : View
 
     [SerializeField] private TextMeshProUGUI _cookiesCountText;
     [SerializeField] private TextMeshProUGUI _cpsText;
-    [SerializeField] private Button _clickButton;
+    [SerializeField] private Button _cookieButton;
     [SerializeField] private Button _openBuildingsButton;
     [SerializeField] private Button _openAchievementsButton;
     [SerializeField] private Button _openUpgradesButton;
@@ -23,7 +25,11 @@ public class CookiesView : View
     [SerializeField] private Button _resetButton;
     [SerializeField] private Button _infoButton;
 
+    [SerializeField] private ClickAnimationSettings clickAnimationSettings;
+
     private readonly CompositeDisposable _disposable = new();
+
+    private Sequence _clickSequence;
 
     [Inject]
     private void Construct(CookiesViewModel cookiesViewModel, IGameProgressSaver gameProgressSaver)
@@ -32,7 +38,7 @@ public class CookiesView : View
         _gameProgressSaver = gameProgressSaver;
 
         _cookiesViewModel.Cookies.Subscribe(_ => _cookiesCountText.text = _.ToString()).AddTo(_disposable);
-        _clickButton.OnClickAsObservable().Subscribe(_ => _cookiesViewModel.ClickCookie()).AddTo(_disposable);
+        _cookieButton.OnClickAsObservable().Subscribe(_ => OnClickCookie()).AddTo(_disposable);
         _openBuildingsButton.OnClickAsObservable().Subscribe(_ => _cookiesViewModel.SwitchBuildingsView());
         _openAchievementsButton.OnClickAsObservable().Subscribe(_ => _cookiesViewModel.SwitchAchievementsView());
         _openUpgradesButton.OnClickAsObservable().Subscribe(_ => _cookiesViewModel.SwitchUpgradesButton());
@@ -40,11 +46,26 @@ public class CookiesView : View
         _saveButton.OnClickAsObservable().Subscribe(_ => _gameProgressSaver.SaveGame());
         _resetButton.OnClickAsObservable().Subscribe(_ => _gameProgressSaver.ResetGameProgress());
         _infoButton.OnClickAsObservable().Subscribe(_ => _cookiesViewModel.SwitchInfoView());
+
+        _clickSequence = DOTween.Sequence();
+        _clickSequence.Append(_cookieButton.transform.DOScale(clickAnimationSettings.TargetScale, clickAnimationSettings.AnimationTime));
+        _clickSequence.Append(_cookieButton.transform.DOScale(1f, clickAnimationSettings.RevertAnimationTime));
+        _clickSequence.OnComplete(() => _clickSequence.Rewind());
     }
 
     private void Update()
     {
         _cookiesViewModel.ProduceCookies(Time.deltaTime);
+    }
+
+    private void OnClickCookie()
+    {
+        _cookiesViewModel.ClickCookie();
+        if(_clickSequence.IsPlaying())
+        { 
+            _clickSequence.Rewind(); 
+        }    
+        _clickSequence.Play();  
     }
 
     private void OnDestroy()
@@ -55,5 +76,13 @@ public class CookiesView : View
     private void UpdateCpS(BigInteger value)
     {
         _cpsText.text = value.ToString(); 
+    }
+
+    [Serializable]
+    public struct ClickAnimationSettings
+    {
+        public float TargetScale;
+        public float AnimationTime;
+        public float RevertAnimationTime;
     }
 }
